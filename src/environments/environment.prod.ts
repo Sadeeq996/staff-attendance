5776988db7edf34764a36b5478fbd054842f56d5
Abubakar Sadeeq <sadeeq996@gmail.com>
2025-11-26 15:57:53 +0100

    add _redirects file for netlify deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // In production default to backend enabled. Change to true to keep mock behaviour in prod builds.
-  useMock: false
+  useMock: true
   ,
   // Set this to your production API base if different (e.g. 'https://api.example.com')
   apiBaseUrl: '/api'
